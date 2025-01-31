2730f10503bc8103f345d1c0c79872f0a2923c5b
Kevin Mittelbach <kevin.mittelbach@crafting-it.de>
2025-01-31 14:29:51 +0100

    Comment out cloud block

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-# Copyright (c) HashiCorp, Inc.
+#Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
 variable "region" {
   description = "AWS region"
-  default     = "us-west-1"
+  default     = "eu-central-1"
 }
 
 variable "instance_type" {
